6886218da137648465aeb50c1503af9cdb286e5d
dalmocabral <dalmo.santos.cabral@gmail.com>
2023-02-25 17:51:15 -0300

    criando a funcao para cal dia do nascimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,26 @@ function calcularEstradaDaVida() {
     const dataNascimento = new Date(document.getElementById('data-nascimento').value);
   
     let dia = (dataNascimento.getDate() + 1).toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-    console.log(dia);
+    //console.log(dia);
     if (dia > 9) {
       dia = dia.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-      console.log(dia);
+      //console.log(dia);
     }
   
     const mes = (dataNascimento.getMonth() + 1).toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-    console.log(mes);
+    //console.log(mes);
     const ano = dataNascimento.getFullYear().toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-    console.log(ano);
+    //console.log(ano);
     let anoReduzido = ano.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-    console.log(anoReduzido);
+    //console.log(anoReduzido);
   
     if (anoReduzido > 9) {
       anoReduzido = anoReduzido.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b));
-      console.log(anoReduzido);
+      //console.log(anoReduzido);
     }
   
     const estradaDaVida = (parseInt(dia) + parseInt(mes) + parseInt(anoReduzido));
-    console.log(estradaDaVida);
+    //console.log(estradaDaVida);
   
     if (estradaDaVida === 11 || estradaDaVida === 22) {
       document.getElementById('resultado').textContent = `${nome}, sua estrada da vida é ${estradaDaVida}.`;
@@ -35,4 +35,28 @@ function calcularEstradaDaVida() {
   document.getElementById('data-nascimento').addEventListener('input', () => {
     calcularEstradaDaVida();
   });
+  
+
+  // FUNÇÃO PARA CALCULAR O DIA DE NASCIMENTO
+  function calcularNumeroReduzidoDia() {
+    const diaString = document.getElementById('data-nascimento').value;
+    const dia = parseInt(diaString.split('-')[2]);
+  
+    let numeroReduzidoDia = dia;
+  
+    while (numeroReduzidoDia > 9) {
+      numeroReduzidoDia = numeroReduzidoDia
+        .toString()
+        .split('')
+        .reduce((a, b) => parseInt(a) + parseInt(b));
+    }
+  
+    if (numeroReduzidoDia !== 11 && numeroReduzidoDia !== 22) {
+      document.getElementById('resultado-dia').textContent = `O número reduzido do dia é ${numeroReduzidoDia}.`;
+    } else {
+      document.getElementById('resultado-dia').textContent = `O número reduzido do dia é ${numeroReduzidoDia} (números mestres não são reduzidos).`;
+    }
+  }
+  
+  document.getElementById('data-nascimento').addEventListener('input', calcularNumeroReduzidoDia);
   
